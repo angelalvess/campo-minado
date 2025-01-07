9afb0ef65e1b3c9998de0d1d1accc3d74929b92a
angelalves <angeltrashangel@gmail.com>
2025-01-07 20:48:37 -0300

    init project

---
+++ src/main/java/com/angel/cm/model/Campo.java
@@ -0,0 +1,12 @@
+package com.angel.cm.model;
+
+public class Campo {
+    private final int row
+    private final int colum
+
+    Campo (int row, int colum) {
+        this.colum = colum;
+        this.row = row;
+    }
+
+}
---
+++ src/test/java/com/angel/cm/Main.java
@@ -0,0 +1,8 @@
+package com.angel.cm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello tranny world, Angie here");
+
+    }
+}
